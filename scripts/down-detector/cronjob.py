40534f3da8c9a4b6982a131e360cb078e3c0598a
Monica Lu <53923036+Monica-Lu@users.noreply.github.com>
2026-02-07 22:31:25 -0500

    script sanity check

--- scripts/down-detector/cronjob.py
+++ scripts/down-detector/cronjob.py
@@ -3,9 +3,10 @@ import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-from dotenv import load_dotenv
+from dotenv import find_dotenv, load_dotenv
 import os
 
+from datetime import datetime, time
 
 def check_website_up(url):
     try:
@@ -59,12 +60,56 @@ def send_alert(url, sender, receiver, password, error_code):
             sender, receiver, message.as_string()
         )
 
+def sanity_check(sender, receiver, password):
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "CRON Job Sanity Check"
+    message["From"] = sender
+    message["To"] = receiver
+
+    text = """\
+        Good Morning,
+        The CRON Job is working fine. *^_^*
+    """
+
+    html = """\
+    <html>
+    <body>
+        <p>Good Morning,<br>
+        The CRON Job is working fine. *^_^*
+        </p>
+    </body>
+    </html>
+    """
+
+    # Turn these into plain/html MIMEText objects
+    part1 = MIMEText(text, "plain")
+    part2 = MIMEText(html, "html")
+
+    # Add HTML/plain-text parts to MIMEMultipart message
+    # The email client will try to render the last part first
+    message.attach(part1)
+    message.attach(part2)
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender, password)
+        server.sendmail(
+            sender, receiver, message.as_string()
+        )
+
 if __name__ == "__main__":
-    load_dotenv()
-    url = "http://localhost:3001/" # Replace with your target URL
+    load_dotenv(find_dotenv())
+    url = os.getenv("url")
     sender_email = os.getenv("sender_email")
-    receiver_emails = ["luq9@rpi.edu", "hemis1227@gmail.com"]
+    receiver_emails = os.getenv("receiver_emails").split(",")
     password = os.getenv("sender_password")
+
+    sent_time = time(hour=22, minute=30)  # Set the time for the sanity check (9:00 AM)
+    current_time = datetime.now()
+    if current_time.hour == sent_time.hour and current_time.minute == sent_time.minute:
+        for receiver in receiver_emails:
+            sanity_check(sender_email, receiver, password)
+    
     if not check_website_up(url)[0]:
         error_code = check_website_up(url)[1]
         for receiver in receiver_emails:
