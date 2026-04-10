15361b742474798279feaf3ef69ab96694cff983
Cooper Werner <cooperwerner824@gmail.com>
2026-04-10 15:58:50 -0400

    making progress

--- src/components/ProfileEditor/sidebar.tsx
+++ src/components/ProfileEditor/sidebar.tsx
@@ -8,6 +8,10 @@ export default function ProfileEditorSidebar() {
   return (
     <div className="w-1/4 flex flex-col bg-bg-base-200 p-4 max-h-full overflow-y-auto ">
       <ProfilePictureEditor />
+      <div>
+        <input type="checkbox" className="toggle toggle-primary" checked />
+        <span className="ml-2 text-sm">Make my profile visible to other users</span>
+      </div>
       <StyledHorizonalSeperator />
       <NameEditor />
       <StyledHorizonalSeperator />
--- src/components/UserProfiles/Sidebar.tsx
+++ src/components/UserProfiles/Sidebar.tsx
@@ -10,89 +10,84 @@ import { Link } from "lucide-react";
 import BeyondButtonLink from "../common/BeyondComponents/BeyondButtonLink";
 
 export default function Sidebar({
-  userProfile,
-  isCurrentUser,
+	userProfile,
+	isCurrentUser,
 }: {
-  userProfile: UserProfile;
-  isCurrentUser: Boolean;
+	userProfile: UserProfile;
+	isCurrentUser: Boolean;
 }) {
-  const [shareToastContent, setShareToastContent] = useState<
-    String | undefined
-  >(undefined);
+	const [shareToastContent, setShareToastContent] = useState<String | undefined>(undefined);
 
-  const handleShareProfile = () => {
-    const profileUrl = `${window.location.origin}/user/${userProfile.rcsid}`;
-    navigator.clipboard
-      .writeText(profileUrl)
-      .then(() => {
-        setShareToastContent("Profile URL copied to clipboard!");
-      })
-      .catch((err) => {
-        console.error("Failed to copy profile URL: ", err);
-        setShareToastContent("Failed to copy profile URL. Please try again.");
-      });
+	const handleShareProfile = () => {
+		const profileUrl = `${window.location.origin}/user/${userProfile.rcsid}`;
+		navigator.clipboard
+			.writeText(profileUrl)
+			.then(() => {
+				setShareToastContent("Profile URL copied to clipboard!");
+			})
+			.catch((err) => {
+				console.error("Failed to copy profile URL: ", err);
+				setShareToastContent("Failed to copy profile URL. Please try again.");
+			});
 
-    // Clear the toast message after 3 seconds
-    setTimeout(() => {
-      setShareToastContent(undefined);
-    }, 3000);
-  };
+		// Clear the toast message after 3 seconds
+		setTimeout(() => {
+			setShareToastContent(undefined);
+		}, 3000);
+	};
 
-  return (
-    <div className="w-1/4 flex flex-col bg-bg-base-200 p-4 grow shrink basis-auto ">
-      {/* Sidebar Information (e.g., contact info, links) can be added here) */}
-      <div className="w-full flex justify-center">
-        <ProfilePicture />
-      </div>
-      <div className="flex w-full justify-between items-center">
-        <h1 className="text-5xl text-primary font-bold">
-          {userProfile.firstName} {userProfile.lastName}
-        </h1>
-        {isCurrentUser && (
-          <BeyondButtonLink
-            href={`/user/${userProfile.rcsid}/edit`}
-            className="text-base h-8 flex items-center"
-          >
-            Edit Profile
-          </BeyondButtonLink>
-        )}
-      </div>
+	return (
+		<div className="w-1/4 flex flex-col bg-bg-base-200 p-4 grow shrink basis-auto ">
+			{/* Sidebar Information (e.g., contact info, links) can be added here) */}
+			<div className="w-full flex justify-center">
+				<ProfilePicture />
+			</div>
+			<div className="flex w-full justify-between items-center">
+				<h1 className="text-5xl text-primary font-bold">
+					{userProfile.firstName} {userProfile.lastName}
+				</h1>
+				{isCurrentUser && (
+					<BeyondButtonLink
+						href={`/user/${userProfile.rcsid}/edit`}
+						className="text-base h-8 flex items-center"
+					>
+						Edit Profile
+					</BeyondButtonLink>
+				)}
+			</div>
 
-      <p className="text-2xl">{userProfile.bio}</p>
-      {userProfile.visibility == "PUBLIC" ? (
-        <BeyondButton onClick={handleShareProfile} className="text-xl mt-2">
-          Share this profile!
-        </BeyondButton>
-      ) : (
-        <p>
-          {" "}
-          This profile is private. Only you, the owner, can view and share
-          it.{" "}
-        </p>
-      )}
-      {shareToastContent && (
-        <div className="bg-base-200 text-primary p-2 rounded-md mt-1">
-          {shareToastContent}
-        </div>
-      )}
-      <StyledHorizonalSeperator />
-      {userProfile.links.length > 0 && (
-        <div>
-          <h2 className="text-3xl font-bold text-primary">Links</h2>
-          <ul>
-            {userProfile.links.map((link, index) => (
-              <li key={index}>
-                <BeyondLink className="text-2xl w-full " href={link.url}>
-                  <div>
-                    <span className="font-semibold">{link.label}</span>
-                    <Link className="inline-block ml-1" size={18} />
-                  </div>
-                </BeyondLink>
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-    </div>
-  );
+			<p className="text-2xl">{userProfile.bio}</p>
+			{userProfile.visibility == "PUBLIC" ? (
+				<BeyondButton onClick={handleShareProfile} className="text-xl mt-2">
+					Share this profile!
+				</BeyondButton>
+			) : (
+				<p>
+					This profile is private. Only you, the owner, can view it. Change your profile visibility
+					in the edit profile page if you want to share it with others.
+				</p>
+			)}
+			{shareToastContent && (
+				<div className="bg-base-200 text-primary p-2 rounded-md mt-1">{shareToastContent}</div>
+			)}
+			<StyledHorizonalSeperator />
+			{userProfile.links.length > 0 && (
+				<div>
+					<h2 className="text-3xl font-bold text-primary">Links</h2>
+					<ul>
+						{userProfile.links.map((link, index) => (
+							<li key={index}>
+								<BeyondLink className="text-2xl w-full " href={link.url}>
+									<div>
+										<span className="font-semibold">{link.label}</span>
+										<Link className="inline-block ml-1" size={18} />
+									</div>
+								</BeyondLink>
+							</li>
+						))}
+					</ul>
+				</div>
+			)}
+		</div>
+	);
 }
